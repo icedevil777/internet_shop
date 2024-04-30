295968f3576e3df8f1e273c3f7d805fe17e29372
Icedevil777 <icedevil777228@mail.ru>
2024-04-30 15:31:50 +0300

    create models

---
+++ main/shop/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class Category(models.Model):
+
+    name = models.CharField('Название категории', max_length=250)
+    alias = models.CharField('Алиас', max_length=200, db_index=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+
+class Category(models.Model):
+    """ Category of products """
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.PROTECT)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
