465f341b797ea28fddc0de67dd4f5c73e48839c3
Icedevil777 <icedevil777228@mail.ru>
2024-05-04 19:49:28 +0300

    add Order Models

--- main/shop/models.py
+++ main/shop/models.py
@@ -8,10 +8,10 @@ class Category(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
-        ordering = ['name']
+        ordering: list[str] = ['name']
         indexes = [models.Index(fields=['name']),]
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        verbose_name: str = 'Категория'
+        verbose_name_plural: str = 'Категории'
 
     def __str__(self) -> str:
         return self.name
@@ -29,9 +29,9 @@ class Product(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['name']
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
+        ordering: list[str] = ['name']
+        verbose_name: str = 'Продукт'
+        verbose_name_plural: str = 'Продукты'
         indexes = [
             models.Index(fields=['id', 'slug']),
             models.Index(fields=['name']),
@@ -42,27 +42,38 @@ class Product(models.Model):
         return self.name
 
 
-# class Order(models.Model):
-#     """
-#     Model for saving data  about orders
-#     """
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
 
-#     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, verbose_name="Покупатель")
+    class Meta:
+        ordering: list[str] = ['-created']
+        indexes: list = [
+            models.Index(fields=['-created']),
+        ]
 
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Покупатель")
-#     data_created = models.DateTimeField(
-#         auto_now_add=True, verbose_name="Дата оформления заказа"
-#     )
+    def __str__(self) -> str:
+        return f'Order {self.id}'
 
-#     products = models.ManyToManyField(Product, verbose_name="Продукты")
+    def get_total_cost(self) -> int:
+        return sum(item.get_cost() for item in self.items.all())
 
-#     @property
-#     def products_count(self) -> int:
-#         return self.products.count()
 
-#     class Meta:
-#         verbose_name = "Заказ"
-#         verbose_name_plural = "Заказы"
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
 
-#     def __str__(self) -> str:
-#         return f"Заказ {self.user}"
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
